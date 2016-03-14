261c5e6a0671d34d37f679d9df257140d8812a6e
Tomorrowman <J.W.Gesink@gmail.com>
2016-03-14 18:56:38 +0100

    Problems fixed

--- src/com/tomorrowman/sorter/SortMyFiles.java
+++ src/com/tomorrowman/sorter/SortMyFiles.java
@@ -30,24 +30,24 @@ public class SortMyFiles implements FileVisitor<Path> {
         matcherGREEN = FileSystems.getDefault().getPathMatcher("glob:*GREEN*");
         matcherBLUE = FileSystems.getDefault().getPathMatcher("glob:*BLUE*");
 
-        if (name != null && matcherRED.matches(name)){
-            System.out.format("Regular file: %s", file);
+        if (name != null && matcherRED.matches(name) && Files.notExists(RED.resolve(file.getFileName()))){
             try {
-                Files.move(file, RED, StandardCopyOption.REPLACE_EXISTING);
+                Files.move(file, RED.resolve(file.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                System.out.format("Moved file: %s", file);
             } catch (IOException e) {
                 System.err.println(e);
             }
-        }else if (name != null && matcherGREEN.matches(name)){
-            System.out.format("Regular file: %s", file);
+        }else if (name != null && matcherGREEN.matches(name) && Files.notExists(GREEN.resolve(file.getFileName()))){
             try {
-                Files.move(file, GREEN, StandardCopyOption.REPLACE_EXISTING);
+                Files.move(file, GREEN.resolve(file.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                System.out.format("Moved file: %s", file);
             } catch (IOException e) {
                 System.err.println(e);
             }
-        }else if (name != null && matcherBLUE.matches(name)) {
-            System.out.format("Regular file: %s", file);
+        }else if (name != null && matcherBLUE.matches(name) && Files.notExists(BLUE.resolve(file.getFileName()))) {
             try {
-                Files.move(file, BLUE, StandardCopyOption.REPLACE_EXISTING);
+                Files.move(file, BLUE.resolve(file.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                System.out.format("Moved file: %s", file);
             } catch (IOException e) {
                 System.err.println(e);
             }
